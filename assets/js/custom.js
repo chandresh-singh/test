5848b5c324707b56d103dd8adfd7ea58f13a560f
chandresh-singh <chandreshkumar684@gmail.com>
2019-07-16 12:14:13 +0530

    Add files via upload
    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+$('.sol-section__slider').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:true
+        }
+    }
+})
